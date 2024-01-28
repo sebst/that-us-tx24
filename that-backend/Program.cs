b23dea26e2922f88704e0833983fb692b1723e85
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-01-28 11:35:40 -0600

    Add cors in backend, and fetch in frontend.

--- that-backend/Program.cs
+++ that-backend/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<ConferenceContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options=> {
+    options.AddPolicy("CorsPolicy", builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
 
 var app = builder.Build();
 
@@ -21,6 +28,7 @@ var app = builder.Build();
 // {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("CorsPolicy");
 // }
 
 // app.UseHttpsRedirection();
--- that-frontend/src/App.tsx
+++ that-frontend/src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sessions, setSessions] = useState(undefined)
+  const updateSessions = async () => {
+    const response = await fetch('https://localhost:7223/api/session')
+    const data = await response.json()
+    setSessions(data)
+  }
+  useEffect(() => {
+    updateSessions()
+  }, [])
+
+  if (sessions===undefined) {
+    return (
+      <>
+        <div>
+          <a href="https://that.us" target="_blank">
+            <img src="/that-conference-texas.avif" className="logo" alt="Vite logo" />
+          </a>
+        </div>
+        <h1>THAT.us Texas 2024</h1>
+        <h2>Loading...</h2>
+      </>
+    )
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://that.us" target="_blank">
+          <img src="/that-conference-texas.avif" className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>THAT.us Texas 2024</h1>
     </>
   )
 }
